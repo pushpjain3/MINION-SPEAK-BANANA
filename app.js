0e209cf0b73a2734b54a2761e4e772925f80623b
Pushp Jain <pushp.ug20@nsut.ac.in>
2021-07-29 03:58:56 +0530

    js refrences created

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Anant ki maa ka bhosda.")
+var btnTranslate = document.querySelector("#btn-translate");
+var hoomanTalk = document.querySelector("#hooman-talk")
+var outputDiv = document.querySelector("#banana-talk")
+btnTranslate.addEventListener("click", (e)=>{
+    outputDiv.innerText = "Translated " + hoomanTalk.value 
+    hoomanTalk.value=''
+})
