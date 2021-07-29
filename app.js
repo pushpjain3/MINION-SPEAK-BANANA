663d2985457486cfefcbdd0807b71afe6142766f
Pushp Jain <pushp.ug20@nsut.ac.in>
2021-07-30 00:01:05 +0530

    added minion api call

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var hoomanTalk = document.querySelector("#hooman-talk")
-var outputDiv = document.querySelector("#banana-talk")
-btnTranslate.addEventListener("click", (e)=>{
-    outputDiv.innerText = "Translated " + hoomanTalk.value 
-    hoomanTalk.value=''
-})
+var hoomanTalk = document.querySelector("#hooman-talk");
+var outputDiv = document.querySelector("#banana-talk");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+btnTranslate.addEventListener("click", () => {
+	var url = serverURL + "?" + "text=" + hoomanTalk.value
+	fetch(url)
+		.then((response) => response.json())
+		.then((json) => {outputDiv.innerText= json.contents.translated})
+        .catch((error)=>{console.log("Error occured! "+error);alert("Something went wrong! Please try again later.")})
+});
