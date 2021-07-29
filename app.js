1b3b7583c48cf4b5e62ead6cc8ca01f511a6f37f
Pushp Jain <pushp.ug20@nsut.ac.in>
2021-07-30 00:40:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ var outputDiv = document.querySelector("#banana-talk");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 btnTranslate.addEventListener("click", () => {
-	var url = serverURL + "?" + "text=" + hoomanTalk.value
+	var url = serverURL + "?" + "text=" + hoomanTalk.value;
+
+    //calling server for processing
 	fetch(url)
 		.then((response) => response.json())
-		.then((json) => {outputDiv.innerText= json.contents.translated})
-        .catch((error)=>{console.log("Error occured! "+error);alert("Something went wrong! Please try again later.")})
+		.then((json) => {
+			outputDiv.innerText = json.contents.translated;
+		})
+		.catch((error) => {
+			console.log("Error occured! " + error);
+			alert("Something went wrong! Please try again later.");
+		});
 });
